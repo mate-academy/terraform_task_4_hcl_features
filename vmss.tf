1f43e4d47512be9a3cdf0395a82f8d14d8acd296
gaupt <olexandrgauptf@gmail.com>
2024-07-20 19:45:11 +0300

    add for_each functions

--- vmss.tf
+++ vmss.tf
@@ -33,3 +33,31 @@ resource "azurerm_virtual_machine" "main" {
     prevent_destroy = true
   }
 }
+
+locals {
+  security_rules = [
+    { name = "rule1", priority = 100, direction = "Inbound", access = "Allow", protocol = "*", source_port_range = "*", destination_port_range = "22", source_address_prefix = "*", destination_address_prefix = "*" },
+    { name = "rule2", priority = 200, direction = "Inbound", access = "Allow", protocol = "*", source_port_range = "*", destination_port_range = "80", source_address_prefix = "*", destination_address_prefix = "*" }
+  ]
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.ts4.location
+  resource_group_name = azurerm_resource_group.ts4.name
+
+  dynamic "security_rule" {
+    for_each = local.security_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
+      destination_address_prefix = security_rule.value.destination_address_prefix
+    }
+  }
+}

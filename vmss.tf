0b27c6a522918a98a823ea63ecd11b6a0ab5760d
gaupt <olexandrgauptf@gmail.com>
2024-07-23 17:29:07 +0300

    add file locals

--- vmss.tf
+++ vmss.tf
@@ -34,20 +34,13 @@ resource "azurerm_virtual_machine" "main" {
   }
 }
 
-locals {
-  security_rules = [
-    { name = "rule1", priority = 100, direction = "Inbound", access = "Allow", protocol = "*", source_port_range = "*", destination_port_range = "22", source_address_prefix = "*", destination_address_prefix = "*" },
-    { name = "rule2", priority = 200, direction = "Inbound", access = "Allow", protocol = "*", source_port_range = "*", destination_port_range = "80", source_address_prefix = "*", destination_address_prefix = "*" }
-  ]
-}
-
 resource "azurerm_network_security_group" "main" {
   name                = "${var.prefix}-nsg"
   location            = azurerm_resource_group.ts4.location
   resource_group_name = azurerm_resource_group.ts4.name
 
   dynamic "security_rule" {
-    for_each = local.security_rules
+    for_each = security_rules.locals
     content {
       name                       = security_rule.value.name
       priority                   = security_rule.value.priority

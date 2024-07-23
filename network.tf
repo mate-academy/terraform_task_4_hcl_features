a08f1d49fcd9afc418d774aae777d86eaed31dc4
gaupt <olexandrgauptf@gmail.com>
2024-07-23 17:48:34 +0300

    comeback variables

--- network.tf
+++ network.tf
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "internal" {
 resource "azurerm_network_interface" "main" {
   count               = length(local.nic_names)
   #name                = "${var.prefix}-nic-${count.index + 1}"
-  name                = local.nic_names [count.index]
+  name                = local.nic_names[count.index]
   location            = azurerm_resource_group.ts4.location
   resource_group_name = azurerm_resource_group.ts4.name
 

e32501bcb86f35a51e6bbd3defc89c56228d8562
IKruglyak <141446484+IKruglyak@users.noreply.github.com>
2024-07-23 20:38:02 +0300

    Update vmss.tf

--- vmss.tf
+++ vmss.tf
@@ -1,24 +1,6 @@
-variable "vm_count" {
-  default = 3
-}
-
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
   location = "West Europe"
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {

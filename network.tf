4d87468f513cc192a341655fd723d830c1e81e13
Ivan Kruglyak <ivankruglyak85@gmail.com>
2024-07-23 20:30:31 +0300

    Solution

---
+++ network.tf
@@ -0,0 +1,35 @@
+resource "azurerm_resource_group" "task4" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.task4.location
+  resource_group_name = azurerm_resource_group.task4.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.task4.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+locals {
+  nic_names = ["nic1", "nic2", "nic3"]
+}
+
+resource "azurerm_network_interface" "main" {
+  count               = length(local.nic_names)
+  name                = "${var.prefix}-nic-${count.index + 1}"
+  location            = azurerm_resource_group.task4.location
+  resource_group_name = azurerm_resource_group.task4.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}

9d8a977ed97b00fb8b7ac4f14f9a4684ab25ba07
IKruglyak <141446484+IKruglyak@users.noreply.github.com>
2024-07-24 14:46:52 +0300

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  network_interface_names = ["nic1", "nic2", "nic3"]
+  nic_names = ["nic1", "nic2", "nic3"]
 
   security_rules = [
     { name = "rule1", priority = 100, direction = "Inbound", access = "Allow", protocol = "*", source_port_range = "*", destination_port_range = "22", source_address_prefix = "*", destination_address_prefix = "*" },

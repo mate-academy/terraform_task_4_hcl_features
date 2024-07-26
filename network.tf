af82fb2a49cf76f6ba6c5d3129351fa51cb498e0
kirillpolozenko <polozenko.kirill.job@gmail.com>
2024-07-26 16:36:48 +0300

    fix

--- network.tf
+++ network.tf
@@ -1,6 +1,5 @@
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  count               = 1
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
@@ -9,6 +8,25 @@ resource "azurerm_virtual_network" "main" {
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main[count.index].name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+
+locals {
+  network_interface_names = ["${var.prefix}-nic-1", "${var.prefix}-nic-2"]
+}
+
+
+resource "azurerm_network_interface" "main" {
+  count               = length(local.network_interface_names)
+  name                = "${var.prefix}-nic-${count.index + 1}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+
+  ip_configuration {
+    name                          = "testconfiguration123"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }

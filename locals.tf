c937d3058ba7fcc999f392987287b929d48cabeb
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-28 20:29:17 +0300

    added terraform infrastructure configuration

---
+++ locals.tf
@@ -0,0 +1,38 @@
+locals {
+  network_interface_names = ["nic-1", "nic-2", "nic-3"]
+  security_rules = [
+    {
+      name                       = "AllowSSH"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "AllowHTTP"
+      priority                   = 200
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "AlloAll"
+      priority                   = 300
+      direction                  = "Outbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+}
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "vm_name" {
+  value = [for vm in azurerm_virtual_machine.main : upper(vm.name)]
+}
+
+output "tags" {
+  value = join(",", [for vm in azurerm_virtual_machine.main : "${vm.tags.environment}-${vm.tags.group}"])
+}
+
+output "vm_ids" {
+  value = [for vm in azurerm_virtual_machine.main : vm.id]
+}

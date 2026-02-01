6bfc942f2ec747fac33ab7c0aaf932616045b599
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-02-01 17:11:54 +0200

    fix: update VM disk name and computer name to include count index for uniqueness

--- vm.tf
+++ vm.tf
@@ -13,13 +13,13 @@ resource "azurerm_virtual_machine" "main" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "myosdisk-vm${count.index + 1}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "hostname"
+    computer_name  = "hostname-vm${count.index + 1}"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }

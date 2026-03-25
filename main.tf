16c0b6cece004d7f86a7c6ccb344938d9556e259
Никита <Goldbergnik1993@gmail.com>
2026-03-25 15:59:50 +0100

    dev

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.105.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 variable "prefix" {
   default = "tfvmex"
+}
+
+locals {
+  nic_names = ["primary", "secondary"]
+  security_rules = [
+    { name = "allow_ssh",   port = 22,  priority = 100 },
+    { name = "allow_http",  port = 80,  priority = 110 },
+    { name = "allow_https", port = 443, priority = 120 }
+  ]
 }
 
 resource "azurerm_resource_group" "example" {
@@ -34,23 +30,46 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_network_security_group" "sg" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  dynamic "security_rule" {
+    for_each = local.security_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = security_rule.value.port
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  }
+}
+
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+  for_each            = toset(local.nic_names)
+  name                = "${var.prefix}-nic-${each.key}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "testconfiguration1"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
+  count                 = 2
+  name                  = "${var.prefix}-vm-${count.index}"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
+  network_interface_ids = [azurerm_network_interface.main[local.nic_names[count.index]].id]
   vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
@@ -60,20 +79,38 @@ resource "azurerm_virtual_machine" "main" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "osdisk-${count.index}"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "hostname"
+    computer_name  = "host-${count.index}"
     admin_username = "testadmin"
     admin_password = "Password1234!"
   }
   os_profile_linux_config {
     disable_password_authentication = false
   }
+
   tags = {
     environment = "staging"
+    project     = "learning"
   }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "vm_name_upper" {
+  value = [for name in azurerm_virtual_machine.main[*].name : upper(name)]
+}
+
+output "tags_joined" {
+  value = join(", ", values(azurerm_virtual_machine.main[0].tags))
+}
+
+output "all_vm_ids" {
+  value = [for vm in azurerm_virtual_machine.main : vm.id]
 }

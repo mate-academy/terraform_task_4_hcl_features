ff8905969b1435f196c3a527d072d40ea8fd6d11
dianahorban <dianahorban477@gmail.com>
2026-07-23 20:09:22 +0200

    The solution

---
+++ locals.tf
@@ -0,0 +1,32 @@
+locals {
+  nic_names = ["nic-1", "nic-2"]
+  nsg_rules = [
+    {
+      name                       = "AllowSSH"
+      priority                   = 1001
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "AllowHTTP"
+      priority                   = 1002
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  ]
+  tags = {
+    environment = "staging"
+    project     = "demo"
+    owner       = "devops"
+  }
+}

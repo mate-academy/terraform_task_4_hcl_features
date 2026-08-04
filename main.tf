bcc32656f46641908ce35b115f5ee7328d596e11
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2026-08-04 15:45:17 +0300

    added main task requirenments

--- main.tf
+++ main.tf
@@ -1,50 +1,39 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.105.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-}
 
-variable "prefix" {
-  default = "tfvmex"
-}
 
 resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "West Europe"
+  count    = 2
+  name     = "${var.prefix}${count.index}-resources"
+  location = "Denmark East"
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
+  dynamic "security_rule" {
+    for_each = var.security_rules
+    content {
+      name                   = security_rule.value.name
+      priority               = security_rule.value.priority
+      direction              = security_rule.value.direction
+      access                 = security_rule.value.access
+      protocol               = security_rule.value.protocol
+      source_port_range      = security_rule.value.source_port_range
+      destination_port_range = security_rule.value.destination_port_range
+    }
   }
 }
+
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
@@ -75,5 +64,8 @@ resource "azurerm_virtual_machine" "main" {
   }
   tags = {
     environment = "staging"
+  }
+  lifecycle {
+    prevent_destroy = true
   }
 }

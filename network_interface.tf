8d4c546ca912ed1d5bddce10d3bb2484a3edf321
Vitalii <stalchenkovitaliy@gmail.com>
2026-09-04 15:29:48 +0400

    added count, for_each, lifecicle, functions for outputs and dynamic block

---
+++ network_interface.tf
@@ -0,0 +1,13 @@
+resource "azurerm_network_interface" "main" {
+  for_each = toset(local.nic_names)
+
+  name                = "${var.prefix}-nic-${each.key}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
